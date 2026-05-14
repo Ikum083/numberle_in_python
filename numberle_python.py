3496f0b65544ee68c5d0c69ef1276fbd7610e5b2
USER <eugenemabansag8@gmail.com>
2026-05-14 22:36:49 +0800

    Added pseudocode

---
+++ numberle_python.py
@@ -0,0 +1,10 @@
+# numberle in python
+# numberle is a game inspired from wordle where instead of guessing a 5 letter word you guess an equation 
+## setup main class
+## setup instance method
+##generate a random number from 001 to 999
+### generate an equation that equals to the random generated number
+### give the user 6 tries to answer the question
+### condition for if user guess the equation in or before 6 turns or fails
+### ask user to continue or end game (setup while loop)
+### keep track of user streak through a text file

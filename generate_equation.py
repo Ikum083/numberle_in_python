3fbb0e59e184a8c22634c302c38467f0cf688429
USER <eugenemabansag8@gmail.com>
2026-05-14 22:51:27 +0800

    Added classes to be used

---
+++ generate_equation.py
@@ -0,0 +1,8 @@
+# import random module
+import random as rnd
+
+# class for generating the equation
+class Equation:
+    def generate_equation(self, number):
+        equations = ["+", "-", "*", "/"]
+        
--- numberle_python.py
+++ numberle_python.py
@@ -1,8 +1,12 @@
 # numberle in python
 # numberle is a game inspired from wordle where instead of guessing a 5 letter word you guess an equation 
+from random import randint
 ## setup main class
+class MainClass:
 ## setup instance method
+    def __init__(self):
 ##generate a random number from 001 to 999
+        self.total = randint(1, 999)
 ### generate an equation that equals to the random generated number
 ### give the user 6 tries to answer the question
 ### condition for if user guess the equation in or before 6 turns or fails
